6531ae13d655a2f5e1f617cccd8f4e693a8de088
Hiromitsu Yamaguchi <yamagame@mac.com>
2020-03-07 17:30:50 +0900

    モジュール更新

---
+++ docs/precache-manifest.94ee5057830a4f5230638d20bc528e4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7977d621e4713c5ae23545c79a35db4",
+    "url": "/yamagame-wallet/index.html"
+  },
+  {
+    "revision": "cfa8025bfa654512ede3",
+    "url": "/yamagame-wallet/static/css/main.a794f530.chunk.css"
+  },
+  {
+    "revision": "4a211a78e324fcf7a0e8",
+    "url": "/yamagame-wallet/static/js/2.4368ac31.chunk.js"
+  },
+  {
+    "revision": "cfa8025bfa654512ede3",
+    "url": "/yamagame-wallet/static/js/main.0d12a737.chunk.js"
+  },
+  {
+    "revision": "f089d91c85e3f818da8d",
+    "url": "/yamagame-wallet/static/js/runtime-main.69fc1f73.js"
+  }
+]);
